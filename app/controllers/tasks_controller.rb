a7178d81fcc446dc1808a3499c1875e80c2eb973
Laurent Curau <laurentcurau@gmail.com>
2013-11-18 16:54:57 -0600

    Add ability to delete tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,10 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def destroy
+    @task = Task.find_by(id: params[:id])
+    @task.destroy
+    redirect_to tasks_url
+  end
+
 end
