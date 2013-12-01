00533af3deb2852eb1528e16d11227b79079503d
Laurent Curau <laurentcurau@gmail.com>
2013-11-30 18:47:55 -0600

    Add Bootstrap navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice: "Welcome back!"
+      redirect_to root_url
     else
       render 'new'
     end
@@ -15,7 +15,8 @@ class SessionsController < ApplicationController
 
   def destroy
     reset_session
-    redirect_to root_url, notice: "Goodbye!"
+    flash[:success] = "You have successfully logged out. Goodbye!"
+    redirect_to root_url
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def flash_class(level)
+    case level
+    when :success then "success"
+    when :notice then "info"
+    when :error then "danger"
+    when :alert then "warning"
+    end
+  end
+
 end
