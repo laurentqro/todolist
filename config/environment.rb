8971d8d9873846e15c87bac53aaf3e00d6ddab5f
Laurent Curau <laurentcurau@gmail.com>
2013-12-01 19:37:55 -0600

    Changes for Heroku staging

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,28 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Todolist::Application.initialize!
 
-CREDENTIALS = YAML.load_file(Rails.root.join('config', 'email.yml'))
+if ENV["RAILS_ENV"] == 'development'
 
-ActionMailer::Base.smtp_settings = {
-  user_name: CREDENTIALS['username'],
-  password: CREDENTIALS['password'],
-  address: 'smtp.gmail.com',
-  port: 587,
-  authentication: 'plain',
-  enable_starttls_auto: true
-}
+  CREDENTIALS = YAML.load_file(Rails.root.join('config', 'email.yml'))
+
+  ActionMailer::Base.smtp_settings = {
+    user_name: CREDENTIALS['username'],
+    password: CREDENTIALS['password'],
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+end
+
+if ENV["RAILS_ENV"] == 'staging'
+
+    ActionMailer::Base.smtp_settings = {
+    user_name: EMAIL_USERNAME,
+    password: EMAIL_PASSWORD,
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+end
